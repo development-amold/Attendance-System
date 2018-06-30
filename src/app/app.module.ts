0995b69b472f9fefba78cf5abccd7775cb692ebc
Amol Dumbre <dev.amold@gmail.com>
2018-06-30 23:54:25 +0530

    Added layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
+import { FooterComponent } from './admin-layout/footer/footer.component';
+import { HeaderComponent } from './admin-layout/header/header.component'; 
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './admin-layout/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
