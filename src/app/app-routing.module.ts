dce009e6d672ecf0e2767a783b5aebaa4dd15ad0
Amol Dumbre <dev.amold@gmail.com>
2018-07-01 00:57:41 +0530

    Added user layouts and missing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,28 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './user-layout/home/home.component';
 import { DashboardComponent } from './admin-layout/dashboard/dashboard.component';
+import { UserLayoutComponent } from './user-layout/user-layout.component';
 
 export const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
+  {
+    path: '', component: UserLayoutComponent,
+    children:[
+      {path: '',redirectTo: "home", pathMatch: "full"},
+      {path: 'home',component: HomeComponent},
+    ]
+  }, // end of frontend
   {
     path: 'admin', component: AdminLayoutComponent,
     children:[ // relative parent path
       {path: '', redirectTo: "dashboard",pathMatch: "full"},
       {path: 'dashboard', component: DashboardComponent},
-      { path: '**', component: PageNotFoundComponent},
+      // { path: '**', component: PageNotFoundComponent},
       // { path: '**', redirectTo: "",pathMatch: "full"}, //--redirect to root path if invalid path found , This wildCard route always be at last
     ]
-  },
+  }, //end of backend
+  { path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 import { FooterComponent } from './admin-layout/footer/footer.component';
 import { HeaderComponent } from './admin-layout/header/header.component'; 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './user-layout/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './admin-layout/dashboard/dashboard.component';
+import { UserLayoutComponent } from './user-layout/user-layout.component';
+import { UserFooterComponent } from './user-layout/user-footer/user-footer.component';
+import { UserHeaderComponent } from './user-layout/user-header/user-header.component';
 
 
 @NgModule({
@@ -19,7 +22,10 @@ import { DashboardComponent } from './admin-layout/dashboard/dashboard.component
     FooterComponent,
     PageNotFoundComponent,
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    UserLayoutComponent,
+    UserFooterComponent,
+    UserHeaderComponent
   ],
   imports: [
     BrowserModule, AppRoutingModule
